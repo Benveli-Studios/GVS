2ea67db70f0ff1ebccecc9312e392bf2788fc5d4
jotaVCBE <jotavcbe2024@gmail.com>
2024-09-02 13:43:26 -0300

    first commit

---
+++ app/src/main/java/co/apps/gvs/Activitys/WelcomeScreen.kt
@@ -0,0 +1,21 @@
+package co.apps.gvs.Activitys
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import co.apps.gvs.R
+
+class WelcomeScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.welcome_screen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
